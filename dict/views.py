d763eec5364b1f02ac7c21b0b3ebcc6959d3af5a
Rishabh Raj <rishabhraj13951@gmail.com>
2024-05-13 12:14:59 +0530

    fetch message usng pyDictionary

--- dict/views.py
+++ dict/views.py
@@ -11,9 +11,12 @@ def search(request):
     search = request.GET.get("search")
     dictionary = PyDictionary()
     meaning = dictionary.meaning(search)
-    synonym = dictionary.synonym(search)
-    antonym = dictionary.antonym(search)
+    synonyms = dictionary.synonym(search)
+    antonyms = dictionary.antonym(search)
 
-    context = {"meaning": meaning, "synonym": synonym, "antonym": antonym}
-
+    context = {
+        "meaning": meaning["Noun"][0],
+        "synonyms": synonyms,
+        "antonyms": antonyms,
+    }
     return render(request, "search.html", context)
