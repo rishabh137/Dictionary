52794c683917a4159b714be1b34f563be22dc50e
Rishabh Raj <rishabhraj13951@gmail.com>
2024-05-16 12:02:23 +0530

    fetch and show data

--- dict/views.py
+++ dict/views.py
@@ -18,37 +18,29 @@ def search(request):
     )  # for antonym and synonym
 
     if res:
-        soup = bs4.BeautifulSoup(res.text, "lxml")
-
-        meaning = soup.find_all("div", {"value": "1"})
+        soup = bs4.BeautifulSoup(res.text, "html.parser")
+        meaning = soup.find_all("div", {"class": "NZKOFkdkcvYgD3lqOIJw"})
         meaning1 = meaning[0].getText()
     else:
-        word = "Sorry, " + word + " Is Not Found In Our Database"
+        search = "Sorry, " + search + " Is Not Found In Our Database"
         meaning = ""
         meaning1 = ""
 
     if res2:
-        soup2 = bs4.BeautifulSoup(res2.text, "lxml")
+        soup2 = bs4.BeautifulSoup(res2.text, "html.parser")
 
-        synonyms = soup2.find_all("a", {"class": "css-r5sw71-ItemAnchor etbu2a31"})
+        # synonyms = soup2.find_all("a", {"class": "css-r5sw71-ItemAnchor etbu2a31"})
+        synonyms = soup2.select(".fltPJVdHfRCxJJVuGX8J a")
         ss = []
         for b in synonyms[0:]:
             re = b.text.strip()
             ss.append(re)
         se = ss
-
-        antonyms = soup2.find_all("a", {"class": "css-lqr09m-ItemAnchor etbu2a31"})
-        aa = []
-        for c in antonyms[0:]:
-            r = c.text.strip()
-            aa.append(r)
-        ae = aa
     else:
         se = ""
-        ae = ""
 
     results = {
-        "word": word,
+        "search": search,
         "meaning": meaning1,
     }
-    return render(request, "search.html", {"se": se, "ae": ae, "results": results})
+    return render(request, "search.html", {"se": se, "results": results})
